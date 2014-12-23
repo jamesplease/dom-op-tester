d12a578d4867b2c7cd8b70f5666f6713fef6d787
Jmeas <jellyes2@gmail.com>
2014-12-22 23:33:34 -0500

    Add test duration.

--- test.js
+++ test.js
@@ -7,13 +7,16 @@
       this.testName = testName;
       observer.start();
       console.log('Starting test ' + testName);
+      this.startTime = performance.now();
       $testBtns.attr('disabled', 'disabled');
     },
     stop: function() {
       var tester = this;
       window.setTimeout(function() {
-        console.log('Stopping test ' + tester.testName + '\n\n');
+        console.log('Stopping test ' + tester.testName);
+        console.log('Total time: ', performance.now() - tester.startTime, '\n\n');
         delete tester.testName;
+        delete tester.startTime;
         observer.stop();
         $testBtns.removeAttr('disabled');
         test.$el.empty().text('The test will be run against this element.');
